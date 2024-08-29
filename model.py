64906f34f21ae8fe3180e8db832cc29c02000166
Xu Tianyang <johnnie.walker@sjtu.edu.cn>
2024-08-29 17:36:30 +0800

    fix: version 1.0.1

---
+++ model.py
@@ -0,0 +1,144 @@
+from mindnlp.peft import (
+    get_peft_config,
+    get_peft_model,
+    LoraConfig,
+    PeftType,
+    PeftConfig,
+    PeftModel,
+)
+import argparse
+import os
+
+import mindspore
+from mindspore.common.api import _no_grad
+
+from tqdm import tqdm
+from mindnlp.core.optim import AdamW
+from mindnlp import evaluate
+from mindnlp.dataset import load_dataset
+from mindnlp.engine import set_seed
+from mindnlp.transformers import AutoModelForSequenceClassification, AutoTokenizer
+from mindnlp.transformers.optimization import get_linear_schedule_with_warmup
+from mindnlp.peft import (
+    get_peft_config,
+    get_peft_model,
+    get_peft_model_state_dict,
+    set_peft_model_state_dict,
+    PeftType,
+    LoraConfig,
+)
+
+import mindspore
+import mindnlp
+from mindnlp.core import no_grad
+from mindnlp.transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSeq2SeqLM
+from mindspore._c_expression import _framework_profiler_step_start
+from mindspore._c_expression import _framework_profiler_step_end
+# 加载tokenizer文件
+
+batch_size = 32
+model_name_or_path = "./.mindnlp/model/ZhipuAI/glm-4-9b-chat"
+task = "mrpc"
+peft_type = PeftType.PROMPT_TUNING
+num_epochs = 20
+peft_config = LoraConfig(task_type="SEQ_2_SEQ_LM", inference_mode=False, r=8, lora_alpha=16, lora_dropout=0.1)
+lr = 3e-4
+max_length = 128
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, mirror='modelscope')
+model = AutoModelForCausalLM.from_pretrained(
+    model_name_or_path,
+    mirror='modelscope',
+    ms_dtype=mindspore.float16,
+).eval()
+
+if any(k in model_name_or_path for k in ("gpt", "opt", "bloom")):
+    padding_side = "left"
+else:
+    padding_side = "right"
+if getattr(tokenizer, "pad_token_id") is None:
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    
+model = get_peft_model(model, peft_config)
+# print(model)
+model.print_trainable_parameters()
+
+import mindspore.dataset as ds
+dataset = ds.CSVDataset(dataset_files="./dataset/output.csv", column_names=['questions', 'answers'], column_defaults=[])
+train_dataset, validation_dataset = dataset.shuffle(64).split([0.9, 0.1])
+
+import numpy as np
+from mindnlp.dataset import BaseMapFunction
+from threading import Lock
+lock = Lock()
+
+class MapFunc(BaseMapFunction):
+    def __call__(self, inputs, outputs):
+        lock.acquire()
+        model_inputs = tokenizer(inputs, max_length=max_length, padding="max_length", truncation=True)
+        labels = tokenizer(outputs, max_length=max_length, padding="max_length", truncation=True)
+        lock.release()
+        labels = labels['input_ids']
+        labels = np.where(np.equal(labels, tokenizer.pad_token_id), -100, labels)
+        return model_inputs['input_ids'], model_inputs['attention_mask'], labels
+
+
+def get_dataset(dataset, tokenizer, shuffle=True):
+    input_colums=['questions', 'answers']
+    output_columns=['input_ids', 'attention_mask', 'labels']
+    dataset = dataset.map(MapFunc(input_colums, output_columns),
+                          input_colums, output_columns)
+    if shuffle:
+        dataset = dataset.shuffle(64)
+    dataset = dataset.batch(batch_size)
+    return dataset
+
+train_dataset = get_dataset(train_dataset, tokenizer)
+eval_dataset = get_dataset(validation_dataset, tokenizer, shuffle=False)
+
+optimizer = mindnlp.core.optim.AdamW(model.parameters(), lr=lr)
+lr_scheduler = get_linear_schedule_with_warmup(
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=(len(train_dataset) * num_epochs),
+)
+
+def forward_fn(**batch):
+    outputs = model(**batch)
+    loss = outputs.loss
+    return loss
+
+grad_fn = mindspore.value_and_grad(forward_fn, None, tuple(model.parameters()))
+
+def train_step(**batch):
+    loss, grads = grad_fn(**batch)
+    optimizer(grads)
+    return loss
+
+for epoch in range(num_epochs):
+    model.set_train()
+    total_loss = 0
+    train_total_size = train_dataset.get_dataset_size()
+    for step, batch in enumerate(tqdm(train_dataset.create_dict_iterator(), total=train_total_size)):
+        loss = train_step(**batch)
+        total_loss += loss.float()
+        lr_scheduler.step()
+
+    model.set_train(False)
+    eval_loss = 0
+    eval_preds = []
+    eval_total_size = eval_dataset.get_dataset_size()
+    for step, batch in enumerate(tqdm(eval_dataset.create_dict_iterator(), total=eval_total_size)):
+        with mindspore._no_grad():
+            outputs = model(**batch)
+        loss = outputs.loss
+        eval_loss += loss.float()
+        eval_preds.extend(
+            tokenizer.batch_decode(ops.argmax(outputs.logits, -1).asnumpy(), skip_special_tokens=True)
+        )
+
+    eval_epoch_loss = eval_loss / len(eval_dataset)
+    eval_ppl = ops.exp(eval_epoch_loss)
+    train_epoch_loss = total_loss / len(train_dataset)
+    train_ppl = ops.exp(train_epoch_loss)
+    print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
---
+++ rag.py
@@ -0,0 +1,161 @@
+from typing import List
+from langchain.embeddings.base import Embeddings
+from mindnlp.sentence import SentenceTransformer
+
+
+class EmbeddingsFunAdapter(Embeddings):
+    def __init__(self, embed_model, mirror='huggingface'):
+        self.embed_model = embed_model
+        self.embedding_model = SentenceTransformer(model_name_or_path=self.embed_model, mirror=mirror)
+
+    def embed_documents(self, texts: List[str]) -> List[List[float]]:
+        embeddings = self.embedding_model.encode_texts(texts)
+        return embeddings
+
+    def embed_query(self, text: str) -> List[float]:
+        embeddings = self.embedding_model.encode_texts([text])
+        return embeddings[0]
+
+import json
+from langchain.document_loaders.base import BaseLoader
+from langchain.schema import Document
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.text_splitter import CharacterTextSplitter
+
+class LineByLineJSONLoader(BaseLoader):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load(self):
+        documents = []
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                json_obj = json.loads(line)
+                # 提取并格式化 JSON 数据
+                content = (
+                    f"Name: {json_obj.get('name', '')}\n"
+                    f"Description: {json_obj.get('desc', '')}\n"
+                    f"Category: {', '.join(json_obj.get('category', []))}\n"
+                    f"Prevent: {json_obj.get('prevent', '')}\n"
+                    f"Cause: {json_obj.get('cause', '')}\n"
+                    f"Symptom: {', '.join(json_obj.get('symptom', []))}\n"
+                    f"Get Probability: {json_obj.get('get_prob', '')}\n"
+                    f"Get Way: {json_obj.get('get_way', '')}\n"
+                    f"Accompany: {', '.join(json_obj.get('acompany', []))}\n"
+                    f"Cure Department: {', '.join(json_obj.get('cure_department', []))}\n"
+                    f"Common Drug: {', '.join(json_obj.get('common_drug', []))}\n"
+                    f"Recommend Drug: {', '.join(json_obj.get('recommand_drug', []))}\n"
+                    f"Easy Get: {', '.join(json_obj.get('easy_get', []))}\n"
+                    f"Medical Insurance status: {json_obj.get('yibao_status', '')}\n"
+                    f"Cure Way: {', '.join(json_obj.get('cure_way', []))}\n"
+                    f"Cure Last Time: {json_obj.get('cure_lasttime', '')}\n"
+                    f"Cured Probability: {json_obj.get('cured_prob', '')}\n"
+                    f"Cost Money: {json_obj.get('cost_money', '')}\n"
+                    f"Check: {', '.join(json_obj.get('check', []))}\n"
+                    f"Recommend Drug: {', '.join(json_obj.get('recommand_drug', []))}\n"
+                    f"Drug Detail: {', '.join(json_obj.get('drug_detail', []))}\n"
+                    f"Do Eat: {', '.join(json_obj.get('do_eat', []))}\n"
+                    f"Don't eat: {', '.join(json_obj.get('not_eat', []))}\n"
+                    f"Recommend Eat: {', '.join(json_obj.get('recommand_eat', []))}\n"
+                )
+                
+                # 创建文档对象
+                documents.append(Document(page_content=content, metadata={'id': json_obj.get('_id', '')}))
+        return documents
+
+# 使用自定义加载器
+loader = LineByLineJSONLoader('dataset/medical.json')
+documents = loader.load()
+
+# 文本切分
+splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+split_documents = []
+for doc in documents:
+    split_docs = splitter.split_text(doc.page_content)
+    for split_doc in split_docs:
+        split_documents.append(Document(page_content=split_doc, metadata=doc.metadata))
+
+import argparse
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import CharacterTextSplitter
+import streamlit as st
+
+import mindspore
+from mindnlp.transformers import AutoTokenizer, AutoModelForCausalLM
+
+def load_knowledge_base(split_documents):
+    texts = [doc.page_content for doc in documents]
+    embeddings = EmbeddingsFunAdapter("AI-ModelScope/m3e-base", mirror='modelscope')
+    faiss = FAISS.from_texts(texts, embeddings)
+    return faiss
+
+
+def load_model_and_tokenizer(model_path):
+    model = AutoModelForCausalLM.from_pretrained(model_path, ms_dtype=mindspore.float16)
+    model.set_train(False)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    return tokenizer, model
+
+# 返回对应的knowledge
+def retrieve_knowledge(faiss, query):
+    docs = faiss.similarity_search(query)
+    return docs[0].page_content
+
+# 将knowlege和query叠加生成input，调用模型chat返回response
+def generate_answer(tokenizer, model, query, knowledge):
+    prompt = f'作为一名顶级医疗助手，你的主要职责是提供精准、专业且富有同理心的医疗建议。你具备全面的医疗知识，能够实时更新并反映最新的医学进展。你的工作方式始终以患者为中心，确保提供最高标准的护理和清晰的沟通。技能：专家诊断与建议，深入分析患者提供的信息，结合最新医学知识进行精准诊断。提出科学合理的治疗建议，确保患者理解并信任你的建议。规则：在任何情况下都不允许破坏角色设定。不说废话，不编造事实。\n\n已知内容:{knowledge}\n\n问题:\n\n{query}'
+    #这一步要再formulate下加个prompt
+    response = model.chat(tokenizer, prompt, temperature=0.001)
+    answer = response[0]
+    return answer
+
+def rag_pipeline(faiss, tokenizer, model, query, use_rag):
+    if use_rag:
+        knowledge = retrieve_knowledge(faiss, query)
+        answer = generate_answer(tokenizer, model, query, knowledge)
+        return answer, knowledge
+    else:
+        answer = generate_answer(tokenizer, model, query, "")
+        return answer, ""
+    
+model_path = "./.mindnlp/model/ZhipuAI/glm-4-9b-chat"
+st.title("RAG Demo")
+faiss = load_knowledge_base(split_documents)
+tokenizer, model = load_model_and_tokenizer(model_path)
+
+if 'answer' not in st.session_state:
+    st.session_state['answer'] = ""
+
+if 'knowledge' not in st.session_state:
+    st.session_state['knowledge'] = ""
+
+with st.form(key='chat_form', clear_on_submit=True):
+    query = st.text_input("输入您的问题：", "")
+    use_rag = st.checkbox("启用检索增强生成 (RAG)", value=True)
+    submitted = st.form_submit_button("发送")
+
+if submitted and query:
+    answer, knowledge = rag_pipeline(faiss, tokenizer, model, query, use_rag)
+    st.session_state['answer'] = answer
+    st.session_state['knowledge'] = knowledge
+
+elif submitted:
+    st.warning("请输入一个问题。")
+
+with st.subheader("用户输入"):
+    st.text_area("User Query", query, height=50)
+
+    # 左右布局
+left_column, right_column = st.columns(2)
+
+with left_column:
+    st.subheader("回答")
+    st.text_area("Assistant Answer", st.session_state['answer'], height=300)
+
+with right_column:
+    st.subheader("知识检索")
+    st.text_area("Knowledge", st.session_state['knowledge'], height=300)
+
+    # 重新运行以刷新对话历史
+if submitted:
+    st.rerun()
